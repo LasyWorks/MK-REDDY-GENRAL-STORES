67ba86131639049ca9b3a19cd9b06c449e1da0b0
Aravind <aanubothu@gmail.com>
2026-03-04 23:17:05 +0530

    debug: add uncaughtException handler to expose startup crash

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,3 +1,14 @@
+// Catch crashes that happen before logger initializes
+process.on('uncaughtException', (err) => {
+  console.error('UNCAUGHT EXCEPTION:', err.message);
+  console.error(err.stack);
+  process.exit(1);
+});
+process.on('unhandledRejection', (reason) => {
+  console.error('UNHANDLED REJECTION:', reason);
+  process.exit(1);
+});
+
 const app = require('./app');
 const config = require('./config');
 const { pool, testConnection } = require('./config/database');
