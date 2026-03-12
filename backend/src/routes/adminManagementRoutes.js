46aeac7ec7e0cb52c766918b0ad380f990e44064
Aravind <aanubothu@gmail.com>
2026-03-12 22:22:12 +0530

    fix: import userController directly in adminManagementRoutes

--- backend/src/routes/adminManagementRoutes.js
+++ backend/src/routes/adminManagementRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const { userController } = require("../controllers");
+const userController = require("../controllers/userController");
 const { authenticate, authorize, superAdminOnly } = require("../middlewares/auth");
 
 // All routes require auth + admin role + super admin privilege
